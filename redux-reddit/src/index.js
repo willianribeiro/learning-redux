cf2351f5345262aada5d3f03edd406bb6c7e184b
Willian Ribeiro <willianribeiro.contato@gmail.com>
2017-11-07 11:17:24 -0200

    Add redux thunks to Reddit example

--- redux-reddit/src/index.js
+++ redux-reddit/src/index.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import thunkMiddleware from 'redux-thunk';
+import { createLogger } from 'redux-logger';
+import { createStore, applyMiddleware } from 'redux';
+import { selectSubreddit, fetchPostsIfNeeded } from './actions';
+import rootReducer from './reducers';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+
+const loggerMiddleware = createLogger();
+const store = createStore(
+  rootReducer,
+  applyMiddleware(
+    thunkMiddleware,
+    loggerMiddleware
+  )
+);
+
+store.dispatch(selectSubreddit('frontend'))
+store.dispatch(fetchPostsIfNeeded('frontend'))
+store.dispatch(fetchPostsIfNeeded('frontend'))
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
