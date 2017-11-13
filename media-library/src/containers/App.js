2df2ecb43c93f53481a0afb9a0b4c3980414f2fe
Willian Ribeiro <willianribeiro.contato@gmail.com>
2017-11-13 17:53:44 -0200

    Add media-library example (redux-saga)

---
+++ media-library/src/containers/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Header from '../common/Header';
+
+// The parent component renders the Header component and component(s) in the
+// route the user navigates to.
+class App extends Component {
+  render() {
+    return (
+      <div className="container-fluid text-center">
+        <Header />
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ media-library/src/sagas/mediaSaga.js
@@ -0,0 +1,24 @@
+import { put, call } from 'redux-saga/effects';
+import { flickrImages, shutterStockVideos } from '../api/api';
+import * as types from '../constants/actionTypes';
+
+// Responsible for searching media library, making calls to the API
+// and instructing the redux-saga middle ware on the next line of action,
+// for success or failure operation.
+export function* searchMediaSaga({ payload }) {
+  try {
+    const [videos, images] = yield [
+      call(shutterStockVideos, payload),
+      call(flickrImages, payload)
+    ]
+
+    yield [
+      put({ type: types.SHUTTER_VIDEOS_SUCCESS, videos }),
+      put({ type: types.SELECTED_VIDEO, video: videos[0] }),
+      put({ type: types.FLICKR_IMAGES_SUCCESS, images }),
+      put({ type: types.SELECTED_IMAGE, image: images[0] })
+    ];
+  } catch (error) {
+    yield put({ type: 'SEARCH_MEDIA_ERROR', error });
+  }
+}
