a4755a0ba4fa848b0143cc4340a1cbfd9f6fafbb
Willian Ribeiro <willianribeiro.contato@gmail.com>
2017-11-14 17:03:57 -0200

    List users on the page (blog project)

--- blog/src/containers/App.js
+++ blog/src/containers/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import { connect } from 'react-redux'
 import * as actions from '../actions'
 
-const App = () => (
+const App = props => (
   <div>
     <button type="button" onClick={actions.fetch_users_if_needed}>Fetch</button>
     <button type="button" onClick={actions.invalidate_users}>Invalidate</button>
+
+    <ul>
+      { props.users.items.map(user => <li key={user.id}>{user.name}</li>) }
+    </ul>
   </div>
 )
 
+const mapStateToProps = state => ({ users: state.users })
 
-export default App;
+export default connect(mapStateToProps)(App);
--- blog/src/index.js
+++ blog/src/index.js
@@ -1,7 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './containers/App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './containers/App'
+import { Provider } from 'react-redux'
+import store from './configureStore'
+import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
+registerServiceWorker()
